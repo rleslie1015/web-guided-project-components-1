d20051441c01e6fc91ab5db9e07d480f3ccac032
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-10-28 12:32:27 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -47,6 +47,7 @@ const accordion = document.querySelector(".accordion");
 // TASK 4- Create a function 'makePanel' that creates a panel exactly as you see it in the HTML.
 function makePanel({ title, content } /* what data does the panel need? */) {
   // TASK 5- Instantiate all the elements needed for a panel
+  debugger;
   const panel = document.createElement("div");
   const panelBar = document.createElement("div");
   const panelContent = document.createElement("div");
